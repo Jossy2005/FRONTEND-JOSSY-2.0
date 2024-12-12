4dc7115942786d1356e2bcb2a465927ff14c22ff
Jossy2005 <jab.mena@yavirac.edu.ec>
2024-12-11 21:39:05 -0500

    first commit

---
+++ src/app/admin/inventario/components/producto/producto.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producto',
+  templateUrl: './producto.component.html',
+  styleUrl: './producto.component.scss'
+})
+export class ProductoComponent {
+  products:any[]=[];
+  cols:any[]=[]
+
+
+
+
+  
+  openNew(){
+
+  }
+}
