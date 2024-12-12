0bb48f37c513bb1ec2453fbe7dd9c043df37880e
AltamiranoYavirac <cav.altamirano@yavirac.edu.ec>
2024-12-12 01:30:23 -0500

    Clase 16

--- src/app/admin/inventario/components/producto/producto.component.ts
+++ src/app/admin/inventario/components/producto/producto.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-producto',
   templateUrl: './producto.component.html',
   styleUrl: './producto.component.scss'
 })
 export class ProductoComponent {
-  products:any[]=[];
+  products:any[]=[{id:0,nombre:"Guapo",precio:394.96,categoria_id:5,stock: 12},
+    {id:1,nombre:"Guapo",precio:394.96,categoria_id:5,stock: 12},
+    {id:2,nombre:"Guapo",precio:394.96,categoria_id:5,stock: 12},
+    {id:3,nombre:"Guapo",precio:394.96,categoria_id:5,stock: 12},
+    {id:4,nombre:"Guapo",precio:394.96,categoria_id:5,stock: 12},
+    {id:5,nombre:"Guapo",precio:394.96,categoria_id:5,stock: 12},
+    {id:6,nombre:"Guapo",precio:394.96,categoria_id:5,stock: 12},
+    {id:7,nombre:"Guapo",precio:394.96,categoria_id:5,stock: 12},
+    {id:8,nombre:"Guapo",precio:394.96,categoria_id:5,stock: 12},
+    {id:9,nombre:"Guapo",precio:394.96,categoria_id:5,stock: 12},
+    {id:10,nombre:"Guapo",precio:394.96,categoria_id:5,stock: 12},
+    {id:11,nombre:"Guapo",precio:394.96,categoria_id:5,stock: 12},
+    {id:12,nombre:"Guapo",precio:394.96,categoria_id:5,stock: 12},
+    {id:13,nombre:"Guapo",precio:394.96,categoria_id:5,stock: 12},
+    {id:14,nombre:"Guapo",precio:394.96,categoria_id:5,stock: 12},
+    {id:15,nombre:"Guapo",precio:394.96,categoria_id:5,stock: 12}
+  ];
   cols:any[]=[]
 
 
@@ -16,4 +31,14 @@ export class ProductoComponent {
   openNew(){
 
   }
+  
+  editProduct(prod:any){
+
+  }
+
+  deleteProduct(prod:any){
+
+  }
+
+
 }
